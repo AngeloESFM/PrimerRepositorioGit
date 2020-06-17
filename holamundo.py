f566ed5a6808c498763d37257231051d7378334e
José Angel <joseangelgonzalezargueta@gmail.com>
2020-06-16 21:19:47 -0500

    segundo cambio

--- holamundo.py
+++ holamundo.py
@@ -1,10 +1,9 @@
 print("Hola mundo")
 for i in range(1, 10):
     print(i, end=", ")
-
-
 i = 1
 while i<10:
     print(i, end=", ")
     i+=1
 
+print("Viendo las diferencias del último cambio guardado")
